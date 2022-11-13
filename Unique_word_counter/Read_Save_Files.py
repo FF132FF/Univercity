61b107605244ef6dc89745dc544b7ee4d99becc2
FF11FF <a11tsdd@gmail.com>
2022-11-13 09:39:52 +0300

    function change in practical work 4

--- Unique_word_counter/Read_Save_Files.py
+++ Unique_word_counter/Read_Save_Files.py
@@ -1,12 +1,18 @@
-def read_file():
-    opened_file = open('data.txt')
-    word_list = list(set(opened_file.read().splitlines()))
-    return word_list
+def read_file(requested_file):
+    content = open(requested_file, "r+")
+    returned_file = list(set(content.read().splitlines()))
+    return returned_file
 
-def save_file(txt, word):
-    words = read_file()
-    words.sort()
-    print(words)
-    count = len(word)
-
-    return count, words
+def save_file(returned_file, requested_file):
+    content = read_file(requested_file)
+    content.sort()
+    count = len(content)
+    words = content
+    words = ' '.join(words)
+    words = words.replace(" ", "\n")
+    text_to_write = count, words
+    returned_file = open(returned_file, mode='w+')
+    returned_file.write(str(count))
+    returned_file.write(str("\n===========\n"))
+    returned_file.write(str(words))
+    return returned_file
--- Unique_word_counter/Unique_word_counter_main.py
+++ Unique_word_counter/Unique_word_counter_main.py
@@ -1,14 +1,7 @@
 from Unique_word_counter import Read_Save_Files as rsf
 
+requested_file = str(input("Enter the name of the file to read and return data: "))
+returned_file = str(input("Enter the name of the file you want to write the data: "))
 
-rsf.read_file()
-FFFFFF = rsf.save_file('data.txt', rsf.read_file())
-count = FFFFFF[0]
-words = FFFFFF[1]
-words = ' '.join(words)
-words = words.replace(" ", "\n")
-text_to_write = count, words
-new_text = open("count.txt", mode = 'w+')
-new_text.write(str(count))
-new_text.write(str("\n===========\n"))
-new_text.write(str(words))
+print(rsf.read_file(requested_file))
+rsf.save_file(returned_file, requested_file)
