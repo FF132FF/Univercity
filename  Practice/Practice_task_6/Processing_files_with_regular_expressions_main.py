f8ae1d06b18c0400615642474290d3e606945243
FF11FF <a11tsdd@gmail.com>
2022-11-23 00:02:41 +0300

    commenting and improving

---  Practice/Practice_task_6/Processing_files_with_regular_expressions_main.py
+++  Practice/Practice_task_6/Processing_files_with_regular_expressions_main.py
@@ -1,5 +1,8 @@
-import Reading_writing_train_log as rw
+import Reading_writing_train_log
+import re
 
 requested_file: str = str(input("Enter the name of the file to read and return data: "))
-returned_file: str = rw.read_log(requested_file)
+returned_file: str = Reading_writing_train_log.read_log(requested_file)
 save_file: str = str(input("Enter the name of the file you want to write the data: "))
+timing: str = re.compile(r'в (\d+:\d+:\d+)')
+print(type(timing))
---  Practice/Practice_task_6/Reading_writing_train_log.py
+++  Practice/Practice_task_6/Reading_writing_train_log.py
@@ -3,9 +3,9 @@ import re
 
 def read_log(requested_file):
     '''
-
-    :param requested_file:
-    :return:
+    Функция принимает название файла; открывает и считывает этот файл; возвращает данные файла в виде текста
+    :param requested_file: название файла (из которого мы возьмем данные)
+    :return: данные из файла (в виде текста)
     '''
     content: TextIOWrapper = open(requested_file, encoding='utf-8')
     returned_file: str = content.read()
@@ -14,12 +14,13 @@ def read_log(requested_file):
 
 def formatting_log(requested_file):
     '''
-
-    :param requested_file:
-    :return:
+    Функция принимает название файла; применяет на него функцию read_log(); форматирует его в список; обрезает список
+    для отображения нужных нам данных; возвращает обрезанный список
+    :param requested_file: название файла (из которого мы возьмем данные)
+    :return: отформатированные данные файла (в виде списка)
     '''
     content: str = read_log(requested_file)
-    content: str = content.splitlines()
+    content: list = content.splitlines()
     del content[2::3]
     del content[1::2]
     return content
@@ -27,19 +28,21 @@ def formatting_log(requested_file):
 
 def save_log(returned_file, requested_file):
     '''
-
-    :param returned_file:
-    :param requested_file:
-    :return:
+    Принимаем название файла; берем данные из этого файла; форматируем их в нужный нам вид; возвращаем отформатированные
+    данные в нужный нам файл (returned_file)
+    :param returned_file: Название файла (в который занесем отформатированные данные)
+    :param requested_file: Название файла (из которого берем данные)
+    :return: файл (с отформатированными данными)
     '''
     content: str = formatting_log(requested_file)
     content: str = ' '.join(content)
-    timing: str = re.compile(r'в (\d+:\d+:\d+)')
-    timing_result: str = timing.findall(content)
-    number: str = re.compile(r'Рейс (\w+)')
-    number_result: str = number.findall(content)
-    place: str = re.compile(r'\d\d\d \w+ (\w+ \w+)')
-    place_result: str = place.findall(content)
+    timing: re.Pattern = re.compile(r'в (\d+:\d+:\d+)')
+    timing_result: list = timing.findall(content)
+    number: re.Pattern = re.compile(r'Рейс (\w+)')
+    number_result: list = number.findall(content)
+    place: re.Pattern = re.compile(r'\d\d\d \w+ (\w+ \w+)')
+    place_result: list = place.findall(content)
+    print(type(place_result))
     returned_file: TextIOWrapper = open(returned_file, mode='w+', encoding='utf-8')
     returned_file.write(str("# File content "))
     returned_file.write(str(returned_file.name))
