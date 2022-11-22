f5cb8cb23f7895c253ee7865e1a5fddab2c3dda0
FF11FF <a11tsdd@gmail.com>
2022-11-22 23:04:33 +0300

    commenting and improving

---  Practice/Practice_task_5/File_conversion.py
+++  Practice/Practice_task_5/File_conversion.py
@@ -1,30 +1,62 @@
 def read_file(requested_file: str):
     '''
-    :param requested_file: Получает название, пытается найти его и прочитать
-    :return: Eсли такой файл есть, возвращает его. Если нету, возвращает специальный файл, который означает ошибку
+    Принимает название файла и пытается открыть этот файл; если файл не открывается и выдает ошибку, то название
+    запрошенного файла изменяется на "ErrorFileNotFound.txt" и возвращает его как ошибку; если файл открывается и не
+    выдает ошибку, считывает файл и записывает числа из файла в список и возвращает этот список
+    :param requested_file: Принимает название файла
+    :return: Eсли такой файл есть, возвращает список чисел в нем; если нет, возвращает специальный файл, который
+    означает ошибку
     '''
     try:
         content: TextIOWrapper = open(requested_file, "r+")
     except:
-        requested_file = "ErrorFileNotFound.txt"
+        requested_file: str = "ErrorFileNotFound.txt"
         return requested_file
     content: TextIOWrapper = open(requested_file, "r+")
-    returned_file = content.read().splitlines()
+    returned_file: list = content.read().splitlines()
     return returned_file
 
 
 def file_conversion(requested_file: str):
     '''
-    :param requested_file: Получает название, пытается найти его и прочитать, затем преобразовывает в нужный формат
-    :return: Eсли файл соотвествует требованиям, возвращает преобразованный файл. Если количество чисел в файле не соответствует первому значению, возвращает ошибку
+    Принимает название файла; пытается найти его и прочитать; если все условия выполнены, преобразовывает в нужный
+    формат и возвращает список чисел; если условия не выполнены, возвращает ошибку
+    :param requested_file: Принимает название файла
+    :return: Eсли файл соотвествует требованиям, возвращает преобразованный список чисел; если количество чисел в файле
+    не соответствует первому значению, возвращает ошибку
     '''
-    returned_file = read_file(requested_file)
-    print(type(returned_file))
-    returned_file = list(map(int, returned_file))
-    save: int = returned_file[0]
+    returned_file: list = read_file(requested_file)
+    returned_file: list = list(map(int, returned_file))
+    amount_of_numbers: int = returned_file[0]
     del returned_file[0]
-    if save == len(returned_file):
+    if amount_of_numbers == len(returned_file):
         return returned_file
     else:
-        broken: str = "Количество чисел не соответствует первому значению!"
-        return
+        broken: str = "Количество чисел не соответствует первому значению"
+        return broken
+
+def error_checking(requested_file):
+    '''
+    Принимает название файла; проводит обнаружение ошибки; если находит, выводит текст с пояснением ошибки; если не
+    находит ошибок, выводит итог работы функции file conversion(): (если файл соотвествует требованиям, возвращает
+    преобразованный список чисел; если количество чисел в файле не соответствует первому значению, возвращает ошибку)
+    :param requested_file: Название файла
+    :return: Возвращает ошибку, если срабатывает except; итог работы функции file_conversion()
+    '''
+    if read_file(requested_file) == "ErrorFileNotFound.txt":
+        print("Ошибка в имени файла")
+    else:
+        try:
+            print(file_conversion(requested_file))
+        except TypeError:
+            print("Неверный тип принимаемых данных")
+        except IndexError:
+            print("Пустой файл")
+        except ValueError:
+            print("Неверный тип данных в файле, введите значения типа Int()")
+        except FileNotFoundError:
+            print("Такого файла нету")
+        except:
+            print("Неизвестная ошибка")
+        finally:
+            print("===========")
