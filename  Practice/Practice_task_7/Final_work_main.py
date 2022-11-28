63ad3e29931e1d4a1d35346c43b957dce1156342
FF11FF <a11tsdd@gmail.com>
2022-11-28 22:01:53 +0300

    commenting and improving

---  Practice/Practice_task_7/Final_work_main.py
+++  Practice/Practice_task_7/Final_work_main.py
@@ -1,9 +1,9 @@
 import Getter
 
-new_list: list = Getter.get_name("Python")
+new_list = Getter.get_name("Python")
 Getter.printlist(new_list)
 
-newer_list: list = Getter.get_list(new_list)
+newer_list = Getter.get_list(new_list)
 print(newer_list)
 
 newest_list = Getter.get_totals(newer_list)
