c846707a7cd04267ee3d66fb7fb0630d7fafa712
FF11FF <a11tsdd@gmail.com>
2022-12-19 07:39:52 +0300

    editing to final format

---  Practice/Practice_task_7/Getting_books_n_totals_main.py
+++  Practice/Practice_task_7/Getting_books_n_totals_main.py
@@ -1,10 +1,9 @@
 import Getting_books_n_totals
 
-
 print("\n==== Generated list of book elements: ")
 print(Getting_books_n_totals.get_list())
 
-keyword: str = "Python"     #str(input("\n==== Write the keyword in lowercase for which you want to find matches: "))
+keyword: str = "Python"  # str(input("\n==== Write the keyword in lowercase for which you want to find matches: "))
 print("\n==== Keyword matches found in this lists ( Keyword ->", keyword, "): ")
 
 print(Getting_books_n_totals.get_books(keyword))
