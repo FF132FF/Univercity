4e7b0daf73989ed522e3d0550d7af082cfd4d425
FFFFFF <a11tsdd@gmail.com>
2023-05-23 14:56:01 +0300

    fix prac №15

---  Practice/Practice_task_15/basicsOfOOP.py
+++  Practice/Practice_task_15/basicsOfOOP.py
@@ -68,6 +68,18 @@ class Student:
         return f"   Имя: {self.name}\n    Фамилия: {self.surname}\n    Средняя оценка за домашние задания: {avgRating}"\
                f"\n    Курсы в процессе изучения: {coursesInProgress}\n    Завершенные курсы: {finishedCourses}"
 
+    def __name__(self):
+        return "Student"
+
+    def __dict__(self):
+        attributeDict = {}
+        attributeDict["name"] = self.name
+        attributeDict["surname"] = self.surname
+        attributeDict["gender"] = self.gender
+        attributeDict["finishedCourses"] = self.finishedCourses
+        attributeDict["coursesInProgress"] = self.coursesInProgress
+        attributeDict["grades"] = self.grades
+        return attributeDict
 
 class Mentor:
     def __init__(self, name, surname):
@@ -85,6 +97,9 @@ class Mentor:
 
     def addCourse(self, course):
         self.coursesAttached.append(course)
+
+    def __name__(self):
+        return "Mentor"
 
 
 class Lecturer(Mentor):
@@ -137,6 +152,16 @@ class Lecturer(Mentor):
         return f"   Имя: {self.name}\n    Фамилия: {self.surname}\n    Курсы: {', '.join(self.coursesAttached)}" \
                f"\n    Средняя оценка за лекции: {avgRating}"
 
+    def __name__(self):
+        return "Lecturer"
+
+    def __dict__(self):
+        attributeDict = {}
+        attributeDict["name"] = self.name
+        attributeDict["surname"] = self.surname
+        attributeDict["grades"] = self.grades
+        attributeDict["coursesAttached"] = self.coursesAttached
+        return attributeDict
 
 
 class Reviewer(Mentor):
@@ -154,6 +179,35 @@ class Reviewer(Mentor):
     def __str__(self):
         return f"   Имя: {self.name}\n    Фамилия: {self.surname}\n    Курсы: {', '.join(self.coursesAttached)}"
 
+    def __name__(self):
+        return "Reviewer"
+    def __dict__(self):
+        attributeDict = {}
+        attributeDict["name"] = self.name
+        attributeDict["surname"] = self.surname
+        attributeDict["coursesAttached"] = self.coursesAttached
+        return attributeDict
+
+
+def avgGradeStudentsOfCourse(studentsList, courseName):
+    grades = []
+    for student in studentsList:
+        if courseName in student.grades:
+            grades.extend(student.grades[courseName])
+    if grades:
+        return sum(grades) / len(grades)
+    else:
+        return
+
+def avgGradeLecturersOfCourse(lecturersList, courseName):
+    grades = []
+    for lecturer in lecturersList:
+        if courseName in lecturer.grades:
+            grades.extend(lecturer.grades[courseName])
+    if grades:
+        return sum(grades) / len(grades)
+    else:
+        return
 
 
 firstStudent = Student("Andrey", "FFFFFF", "male")
@@ -205,53 +259,86 @@ secondStudent.rateLecturer(secondLecturer, "c++", 9)
 
 print("\n    ======================================================================================================="
       "=======    \n")
-print("Метод student.__str__() выводит основную информацию об экземпляре класса Student:\n", firstStudent.__str__())
-print("Метод student.__int__() выводит общее количество оценок по всем курсам экзмепляра класса Student:\n   ",
+print("Метод student.__name__() выводит имя класса:\n   ", firstStudent.__name__())
+print("\nМетод student.__dict__() выводит словарь с атрибутами класса:\n   ", firstStudent.__dict__())
+print("\nМетод student.__str__() выводит основную информацию об экземпляре класса Student:\n", firstStudent.__str__())
+print("\nМетод student.__int__() выводит общее количество оценок по всем курсам экзмепляра класса Student:\n   ",
       firstStudent.__int__())
-print("Метод student.__bool__() выводит True, если общее количество оценок по всем курсам экзмепляра класса Student не "
+print("\nМетод student.__bool__() выводит True, если общее количество оценок по всем курсам экзмепляра класса Student не "
       "четное, и False, если четное:\n   ", firstStudent.__bool__())
-print("Метод student.__complex__() приводит общее количество оценок по всем курсам экзмепляра класса Student к "
+print("\nМетод student.__complex__() приводит общее количество оценок по всем курсам экзмепляра класса Student к "
       "комплексному типу:\n   ", firstStudent.__complex__())
-print("Метод student.__float__() выводит среднюю оценку за домашние задания по всем курсам экзмепляра класса Student "
+print("\nМетод student.__float__() выводит среднюю оценку за домашние задания по всем курсам экзмепляра класса Student "
       ":\n   ", firstStudent.__float__())
 print("\n    ======================================================================================================="
       "=======    \n")
-print("Метод student.__str__() выводит основную информацию об экземпляре класса Student:\n", secondStudent.__str__())
-print("Метод student.__int__() выводит общее количество оценок по всем курсам экзмепляра класса Student:\n   ",
+print("Метод student.__name__() выводит имя класса:\n   ", secondStudent.__name__())
+print("\nМетод student.__dict__() выводит словарь с атрибутами класса:\n   ", secondStudent.__dict__())
+print("\nМетод student.__str__() выводит основную информацию об экземпляре класса Student:\n", secondStudent.__str__())
+print("\nМетод student.__int__() выводит общее количество оценок по всем курсам экзмепляра класса Student:\n   ",
       secondStudent.__int__())
-print("Метод student.__bool__() выводит True, если общее количество оценок по всем курсам экзмепляра класса Student не "
+print("\nМетод student.__bool__() выводит True, если общее количество оценок по всем курсам экзмепляра класса Student не "
       "четное, и False, если четное:\n   ", secondStudent.__bool__())
-print("Метод student.__complex__() приводит общее количество оценок по всем курсам экзмепляра класса Student к "
+print("\nМетод student.__complex__() приводит общее количество оценок по всем курсам экзмепляра класса Student к "
       "комплексному типу:\n   ", secondStudent.__complex__())
-print("Метод student.__float__() выводит среднюю оценку за домашние задания по всем курсам экзмепляра класса Student "
+print("\nМетод student.__float__() выводит среднюю оценку за домашние задания по всем курсам экзмепляра класса Student "
       ":\n   ", secondStudent.__float__())
 print("\n    ======================================================================================================="
       "=======    \n")
-print("Метод lecturer.__str__() выводит основную информацию об экземпляре класса Lecturer:\n", firstLecturer.__str__())
-print("Метод lecturer.__int__() выводит общее количество оценок по всем курсам экзмепляра класса Lecturer:\n   ",
+print("Метод lecturer.__name__() выводит имя класса:\n   ", firstLecturer.__name__())
+print("\nМетод lecturer.__dict__() выводит словарь с атрибутами класса:\n   ", firstLecturer.__dict__())
+print("\nМетод lecturer.__str__() выводит основную информацию об экземпляре класса Lecturer:\n", firstLecturer.__str__())
+print("\nМетод lecturer.__int__() выводит общее количество оценок по всем курсам экзмепляра класса Lecturer:\n   ",
       firstLecturer.__int__())
-print("Метод lecturer.__bool__() выводит True, если общее количество оценок по всем курсам экзмепляра класса Lecturer "
+print("\nМетод lecturer.__bool__() выводит True, если общее количество оценок по всем курсам экзмепляра класса Lecturer "
       "не четное, и False, если четное:\n   ", firstLecturer.__bool__())
-print("Метод lecturer.__complex__() приводит общее количество оценок по всем курсам экзмепляра класса Lecturer к "
+print("\nМетод lecturer.__complex__() приводит общее количество оценок по всем курсам экзмепляра класса Lecturer к "
       "комплексному типу:\n   ", firstLecturer.__complex__())
-print("Метод lecturer.__float__() выводит среднюю оценку за домашние задания по всем курсам экзмепляра класса Lecturer "
+print("\nМетод lecturer.__float__() выводит среднюю оценку за домашние задания по всем курсам экзмепляра класса Lecturer "
       ":\n   ", firstLecturer.__float__())
 print("\n    ======================================================================================================="
       "=======    \n")
-print("Метод lecturer.__str__() выводит основную информацию об экземпляре класса Lecturer:\n", secondLecturer.__str__())
-print("Метод lecturer.__int__() выводит общее количество оценок по всем курсам экзмепляра класса Lecturer:\n   ",
+print("Метод lecturer.__name__() выводит имя класса:\n   ", secondLecturer.__name__())
+print("\nМетод lecturer.__dict__() выводит словарь с атрибутами класса:\n   ", secondLecturer.__dict__())
+print("\nМетод lecturer.__str__() выводит основную информацию об экземпляре класса Lecturer:\n", secondLecturer.__str__())
+print("\nМетод lecturer.__int__() выводит общее количество оценок по всем курсам экзмепляра класса Lecturer:\n   ",
       secondLecturer.__int__())
-print("Метод lecturer.__bool__() выводит True, если общее количество оценок по всем курсам экзмепляра класса Lecturer "
+print("\nМетод lecturer.__bool__() выводит True, если общее количество оценок по всем курсам экзмепляра класса Lecturer "
       "не четное, и False, если четное:\n   ", secondLecturer.__bool__())
-print("Метод lecturer.__complex__() приводит общее количество оценок по всем курсам экзмепляра класса Lecturer к "
+print("\nМетод lecturer.__complex__() приводит общее количество оценок по всем курсам экзмепляра класса Lecturer к "
       "комплексному типу:\n   ", secondLecturer.__complex__())
-print("Метод lecturer.__float__() выводит среднюю оценку за домашние задания по всем курсам экзмепляра класса Lecturer "
+print("\nМетод lecturer.__float__() выводит среднюю оценку за домашние задания по всем курсам экзмепляра класса Lecturer "
       ":\n   ", secondLecturer.__float__())
 print("\n    ======================================================================================================="
       "=======    \n")
-print("Метод reviewer.__str__() выводит основную информацию об экземпляре класса Reviewer:\n", firstReviewer.__str__())
+print("Метод reviewer.__name__() выводит имя класса:\n    ", firstReviewer.__name__())
+print("\nМетод reviewer.__dict__() выводит словарь с атрибутами класса:\n   ", firstReviewer.__dict__())
+print("\nМетод reviewer.__str__() выводит основную информацию об экземпляре класса Reviewer:\n", firstReviewer.__str__())
 print("\n    ======================================================================================================="
       "=======    \n")
-print("Метод reviewer.__str__() выводит основную информацию об экземпляре класса Reviewer:\n", secondReviewer.__str__())
+print("Метод reviewer.__name__() выводит имя класса:\n   ", secondReviewer.__name__())
+print("\nМетод reviewer.__dict__() выводит словарь с атрибутами класса:\n   ", secondReviewer.__dict__())
+print("\nМетод reviewer.__str__() выводит основную информацию об экземпляре класса Reviewer:\n", secondReviewer.__str__())
+print("\n    ======================================================================================================="
+      "=======    \n")
+
+studentList = [firstStudent, secondStudent]
+lecturerList = [firstLecturer, secondLecturer]
+
+print("Пример работы метода avgGradeStudentOfCouse, который высчитывает среднюю оценку за домашнее задание по всем "
+      "студентам в рамках конкретного курса: \n")
+print("    Средняя оценка за домашнее задание по всем студентам в рамках конкретного курса ==python== :",
+      avgGradeStudentsOfCourse(studentList, "python"))
+print("    Средняя оценка за домашнее задание по всем студентам в рамках конкретного курса ==JS== :",
+      avgGradeStudentsOfCourse(studentList, "JS"))
+print("    Средняя оценка за домашнее задание по всем студентам в рамках конкретного курса ==c++== :",
+      avgGradeStudentsOfCourse(studentList, "c++"))
+print("\n    ======================================================================================================="
+      "=======    \n")
+print("Пример работы метода avgGradeLecturerOfCouse, который высчитывает среднюю оценку за лекции всех лекторов в "
+      "рамках конкретного курса: \n")
+print("    Средняя оценка за лекции всех лекторов в рамках конкретного курса ==python== :", avgGradeLecturersOfCourse(lecturerList, "python"))
+print("    Средняя оценка за лекции всех лекторов в рамках конкретного курса ==JS== :", avgGradeLecturersOfCourse(lecturerList, "JS"))
+print("    Средняя оценка за лекции всех лекторов в рамках конкретного курса ==c++== :", avgGradeLecturersOfCourse(lecturerList, "c++"))
 print("\n    ======================================================================================================="
       "=======    \n")
