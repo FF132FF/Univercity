6f17a85b7bfd3dab380c4ce41cc18f0329d3e14b
FFFFFF <a11tsdd@gmail.com>
2023-05-23 10:28:29 +0300

    add test and __int__ __float__ __bool__ __complex__

---  Practice/Practice_task_15/basicsOfOOP.py
+++  Practice/Practice_task_15/basicsOfOOP.py
@@ -32,6 +32,35 @@ class Student:
         return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {avgRating}" \
                f"\nКурсы в процессе изучения: {coursesInProgress}\nЗавершенные курсы: {finishedCourses}"
 
+    def __int__(self):
+        return len(self.grades)
+
+    def __float__(self):
+        sum = 0
+        if len(self.grades) != 0:
+            for grades in self.grades.values():
+                for grade in grades:
+                    sum += grade
+            avr = sum / len(self.grades)
+        return avr
+
+    def __bool__(self):
+        if len(self.grades) % 2 == 1:
+            return True
+        else:
+            return False
+
+    def __complex__(self):
+        mn = 11.0
+        mx = -1.0
+        for grades in self.grades.values():
+            for grade in grades:
+                if grade > mx:
+                    mx = grade
+                if grade < mn:
+                    mn = grade
+        z = complex(mx, mn)
+        return z
 
 class Mentor:
     def __init__(self, name, surname):
@@ -41,6 +70,7 @@ class Mentor:
 
     def rateStudent(self, student, course, grade):
         student.grades[course] = [grade]
+
 
     def addCourse(self, course):
         self.coursesAttached.append(course)
@@ -63,9 +93,39 @@ class Lecturer(Mentor):
             return "Не оценен"
         return round(sum / count, 3)
 
+    def __int__(self):
+        return len(self.grades)
+
+    def __float__(self):
+        sum = 0
+        if len(self.grades) != 0:
+            for grades in self.grades.values():
+                for grade in grades:
+                    sum += grade
+            avr = sum / len(self.grades)
+        return avr
+
+    def __bool__(self):
+        if len(self.grades) % 2 == 1:
+            return True
+        else:
+            return False
+
+    def __complex__(self):
+        mn = 11.0
+        mx = -1.0
+        for grades in self.grades.values():
+            for grade in grades:
+                if grade > mx:
+                    mx = grade
+                if grade < mn:
+                    mn = grade
+        z = complex(mx, mn)
+        return z
     def __str__(self):
         avgRaiting = self.averageRaiting()
         return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {avgRaiting}"
+
 
 
 class Reviewer(Mentor):
@@ -74,9 +134,63 @@ class Reviewer(Mentor):
         self.coursesAttached = []
 
     def rateStudent(self, student, course, grade):
-        if course in self.coursesAttached and course in student.coursesInProgress:
+        if course in self.coursesAttached and ((course in student.coursesInProgress)
+                                               or (course in student.finishedCourses)):
             student.grades[course] = [grade]
 
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}"
 
+
+
+firstStudent = Student("Andrey", "Andrey", "male")
+secondStudent = Student("Pavel", "Pavel", "male")
+
+firstReviewer = Reviewer("Ilya", "Ilya")
+firstReviewer.addCourse("python")
+firstReviewer.addCourse("c++")
+firstReviewer.addCourse("JS")
+firstStudent.addCourse("python")
+firstStudent.addCourse("c++")
+firstStudent.addCourse("JS")
+secondStudent.addCourse("python")
+secondStudent.addCourse("c++")
+firstReviewer.rateStudent(secondStudent, "python", 3)
+firstReviewer.rateStudent(secondStudent, "c++", 8)
+firstReviewer.rateStudent(firstStudent, "python", 7)
+firstReviewer.rateStudent(firstStudent, "c++", 7)
+firstReviewer.rateStudent(firstStudent, "JS", 9)
+
+firstLecturer = Lecturer("Alexey", "Alexey")
+firstLecturer.addCourse("python")
+firstLecturer.addCourse("c++")
+firstLecturer.addCourse("JS")
+
+firstStudent.rateLecturer(firstLecturer, "JS", 9)
+firstStudent.rateLecturer(firstLecturer, "c++", 6)
+firstStudent.rateLecturer(firstLecturer, "python", 8)
+secondStudent.rateLecturer(firstLecturer, "python", 3)
+secondStudent.rateLecturer(firstLecturer, "c++", 9)
+
+
+
+
+
+
+print(firstStudent.__str__())
+print(firstStudent.__int__())
+print(firstStudent.__bool__())
+print(firstStudent.__complex__())
+print(firstStudent.__float__())
+print("  ===========")
+print(secondStudent.__str__())
+print(secondStudent.__int__())
+print(secondStudent.__bool__())
+print(secondStudent.__complex__())
+print(secondStudent.__float__())
+print("  ===========")
+print(firstLecturer.__str__())
+print(firstLecturer.__int__())
+print(firstLecturer.__bool__())
+print(firstLecturer.__complex__())
+print(firstLecturer.__float__())
